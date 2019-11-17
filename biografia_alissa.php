214010d5a4d94e404fee60c425bd3716d1038e50
Brahian Mejia <brayantqm123@gmail.com>
2019-11-17 12:50:28 -0500

    Subida del primer proyecto de html con css.

---
+++ biografia_alissa.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Biografia de Alissa</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+
+<body>
+	<h1>Biografia de Alissa</h1>
+	<p>Alissa White-Gluz (Montreal, 31 de julio de 1985) es la actual vocalista de la banda de death metal melódico Arch Enemy. Alissa es una de las pocas mujeres vocalistas que usan voces guturales como su estilo principal de canto. Tiene la cualidad de cambiar radicalmente de tipo de voz sin ninguna dificultad. Alissa fue una de las fundadoras de la banda «The Tempest» que después se cambiaría el nombre por «The Agonist».</p>
+
+	<p>Alissa solía mezclar en su anterior grupo (The Agonist) los guturales con voz limpia, haciendo una alternancia virtuosa en directo entre ambos estilos. Esta forma de canto la abandonó en Arch Enemy, ya que en su primer álbum con el grupo (War Eternal) solo utiliza guturales y únicamente en la canción "Avalanche" se escucha de fondo la voz en limpio de Alissa en una parte del estribillo.</p>
+
+	<p>Alissa en una entrevista dijo que le gustaría usar voz melódica en futuros trabajos en Arch Enemy. En cuanto a su participación con Kamelot su voz varió desde el canto popular, gutural y lírico, en el cual asume un rol mayoritariamente de corista y apoyo de Mezzosoprano en las partes vocales femeninas de la banda, dice que al cantar con Kamelot siente que esta actuando en una obra de teatro en el cual interpreta un papel.</p>
+	<br>
+	<img id="imagen2" src="images/biografia_alissa.jpg">
+	<br>
+	<a href="index.php">Volver</a>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Hola Mundo</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+
+<body>
+	<!--Esto es un comentario en Html-->
+	<h1>Alissa White-Gluz</h1>
+	<a href="biografia_alissa.php"><img id="imagen1" src="images/alissa.jpg"></a>
+</body>
+</html>
